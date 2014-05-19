d946da22df53969799970fa17aeb38d35f0aed7e
sekito2000 <sekito2000@r2.ucom.ne.jp>
2014-05-19 22:12:52 +0900

    ｲﾝﾎﾟｰﾄ
    初回ｲﾝﾎﾟｰﾄ分です。

---
+++ test_tasktray/test_tasktray/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+		Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+		Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+		Me.終了ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+		Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+		Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+		Me.ContextMenuStrip1.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'NotifyIcon1
+		'
+		Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+		Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+		Me.NotifyIcon1.Text = "SendMail"
+		Me.NotifyIcon1.Visible = True
+		'
+		'ContextMenuStrip1
+		'
+		Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.終了ToolStripMenuItem})
+		Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+		Me.ContextMenuStrip1.Size = New System.Drawing.Size(101, 26)
+		'
+		'終了ToolStripMenuItem
+		'
+		Me.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem"
+		Me.終了ToolStripMenuItem.Size = New System.Drawing.Size(100, 22)
+		Me.終了ToolStripMenuItem.Text = "終了"
+		'
+		'Timer1
+		'
+		Me.Timer1.Interval = 5000
+		'
+		'Timer2
+		'
+		Me.Timer2.Interval = 60000
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(284, 262)
+		Me.Name = "Form1"
+		Me.Text = "Form1"
+		Me.ContextMenuStrip1.ResumeLayout(False)
+		Me.ResumeLayout(False)
+
+	End Sub
+	Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+	Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+	Friend WithEvents 終了ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+	Friend WithEvents Timer1 As System.Windows.Forms.Timer
+	Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ test_tasktray/test_tasktray/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Security.Permissions
+
+Public Class Form1
+
+	'【フォームを表示しないようにする方法】
+	'フォームのCreateParamsプロパティをオーバーライドする
+	Protected Overrides ReadOnly Property CreateParams() As CreateParams
+		<SecurityPermission(SecurityAction.Demand, _
+		 Flags:=SecurityPermissionFlag.UnmanagedCode)> _
+		Get
+			Const WS_EX_TOOLWINDOW As Int32 = &H80
+			Const WS_POPUP As Int32 = &H80000000
+			Const WS_VISIBLE As Int32 = &H10000000
+			Const WS_SYSMENU As Int32 = &H80000
+			Const WS_MAXIMIZEBOX As Int32 = &H10000
+
+			Dim cp As System.Windows.Forms.CreateParams
+			cp = MyBase.CreateParams
+			cp.ExStyle = WS_EX_TOOLWINDOW
+			cp.Style = WS_POPUP Or WS_VISIBLE Or _
+			 WS_SYSMENU Or WS_MAXIMIZEBOX
+			cp.Height = 0
+			cp.Width = 0
+			Return cp
+		End Get
+	End Property
+
+
+	Private Sub 終了ToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles 終了ToolStripMenuItem.Click
+		NotifyIcon1.Visible = False		'アイコンをトレイから取り除く
+		Timer1.Enabled = False			'[ｵﾌ]メール送信ﾁｪｯｸﾀｲﾏー
+		Timer2.Enabled = False			'[ｵﾌ]重複送防止用ﾀｲﾏー
+		Application.Exit()				'[終了]ｱﾌﾟﾘ
+	End Sub
+
+	Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+		'MessageBox.Show("aaa")
+		Timer1.Enabled = True			'[ｵﾝ]メール送信ﾁｪｯｸﾀｲﾏー
+		Timer2.Enabled = False			'[ｵﾌ]重複送防止用ﾀｲﾏー
+	End Sub
+
+	'[ﾀｲﾏｰ]メール送信ﾁｪｯｸ≪ｲﾝﾀｰﾊﾞﾙ：５秒≫
+	Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+		Dim stHHmm As String
+		stHHmm = Format(Now, "HH:mm")
+		'[一致]メール送信時間
+		If stHHmm = "22:12" Then
+			Timer1.Enabled = False	'[ｵﾌ]メール送信ﾁｪｯｸﾀｲﾏー
+			Timer2.Enabled = True	'[ｵﾝ]重複送防止用ﾀｲﾏー
+
+			'[メール送信]
+			MessageBox.Show("22:12です≪メール送信します≫")
+		End If
+	End Sub
+
+	'[ﾀｲﾏｰ]重複送防止用≪ｲﾝﾀｰﾊﾞﾙ：１分≫
+	Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+		Timer2.Enabled = False	'[ｵﾌ]重複送防止用ﾀｲﾏー
+		Timer1.Enabled = True	'[ｵﾝ]メール送信ﾁｪｯｸﾀｲﾏー
+	End Sub
+End Class
